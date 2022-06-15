628fa2f19c841a23588ce754ebcf0e65ed14ec22
yuna8815 <yuna.kim@smd-sol.com>
2022-06-15 14:03:01 +0900

    Nav add

--- src/components/widget/SideNavXs.vue
+++ src/components/widget/SideNavXs.vue
@@ -4,35 +4,86 @@
     absolute
     temporary
     class="pa-4">
-    <div class="px-2 py-4">
-      <UserAvatar />
+    <div class="d-flex fill-height flex-column align-center justify-space-between">
+      <div style="width: 100%;">
+        <div class="px-2 py-4"><UserAvatar /></div>
+
+        <v-divider class="my-4" />
+
+        <v-list
+          nav
+          width="100%"
+          class="pa-0">
+          <v-list-item-group color="primary">
+            <v-list-item
+              link
+              to="/form/inbox"
+              class="mb-6 px-4">
+              <v-list-item-icon class="mr-4"><edit-2-icon size="24"></edit-2-icon></v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>작성 목록</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item
+              link
+              to="/form/outbox"
+              class="mb-6 px-4">
+              <v-list-item-icon class="mr-4"><list-icon size="24"></list-icon></v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>의뢰 목록</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-group
+              :value="true"
+              no-action>
+              <template v-slot:activator>
+                <v-list-item class="px-4">
+                  <v-list-item-icon class="mr-4"><settings-icon size="24"></settings-icon></v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>등록 관리</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                link
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list-group>
+          </v-list-item-group>
+        </v-list>
+      </div>
+      
+      <v-btn
+        depressed
+        rounded
+        color="primary"
+        to="/">
+        로그아웃
+      </v-btn>
     </div>
-
-    <v-divider class="my-4" />
-
-    <v-list nav>
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        link>
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
 import UserAvatar from '@/components/widget/UserAvatar'
 
+import { Edit2Icon, ListIcon, SettingsIcon } from 'vue-feather-icons'
+
 export default {
   components: {
-    UserAvatar
+    UserAvatar,
+    Edit2Icon,
+    ListIcon,
+    SettingsIcon
   },
   props: {
     params: Object
@@ -40,9 +91,9 @@ export default {
   data() {
     return {
       items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' },
+        { title: '직업병 안심센터 관리' },
+        { title: '협력병원 관리' },
+        { title: '의료진 관리' },
       ],
     }
   }
